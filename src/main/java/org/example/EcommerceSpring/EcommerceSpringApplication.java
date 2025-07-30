2954e952f229396dcfad7814bbdfbdf794c43f64
Lakshya <lakshay462@gmail.com>
2025-07-30 11:19:08 +0530

    Added dotenv basic setup

--- src/main/java/org/example/EcommerceSpring/EcommerceSpringApplication.java
+++ src/main/java/org/example/EcommerceSpring/EcommerceSpringApplication.java
@@ -1,5 +1,7 @@
 package org.example.EcommerceSpring;
 
+import io.github.cdimascio.dotenv.Dotenv;
+import io.github.cdimascio.dotenv.DotenvEntry;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +9,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class EcommerceSpringApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.configure().load();
+
+		dotenv.entries().forEach((DotenvEntry entry)  -> System.setProperty(entry.getKey(), entry.getValue()));
 		SpringApplication.run(EcommerceSpringApplication.class, args);
 	}
 
